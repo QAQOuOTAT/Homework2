2e3c921fa1724ffcbd638f436b77e9729483f732
QAQOuOTAT <161003276+QAQOuOTAT@users.noreply.github.com>
2024-03-05 01:17:23 +0800

    Add files via upload

--- hw2_2.py
+++ hw2_2.py
@@ -4,14 +4,21 @@ Created on Tue Mar  5 00:05:59 2024
 
 @author: LAM Quincy
 """
-n=str(input('please input your name: '))
-a=int(input('please input your age: '))
-h=float(input('please input your hight: '))
-c=str(input('please input your favorite color: '))
-if h>100:
-    h1=h/100
-    print(n,",",a,"years old , ",h1,"m"," , favorite color is ",c)
-elif h<=0:
-    print(n,",",a,"years old , favorite color is ",c,'\nAlso please input correct hight')
-else :
-    print(n,",",a,"years old , ",h,"m"," , favorite color is ",c)
+print('========================','\ntype x to exit','\n========================')
+while True:
+    n=str(input('please input your name: '))
+    if n == 'x':
+        print('Exit','Thanks for using')
+        break
+    
+    a=int(input('please input your age: '))
+    h=float(input('please input your hight: '))
+    c=str(input('please input your favorite color: '))
+    
+    if h>100:
+        h1=h/100
+        print(n,",",a,"years old , ",h1,"m"," , favorite color is ",c,'\n========================')
+    elif h<=0:
+        print(n,",",a,"years old , favorite color is ",c,'\nAlso please input correct hight','\n========================')
+    else :
+        print(n,",",a,"years old , ",h,"m"," , favorite color is ",c,'\n========================')
